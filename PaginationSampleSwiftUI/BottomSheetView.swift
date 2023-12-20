111701ce1bfc1e0b6a46c9121615670d5ac415a7
Chandra Sekhar <chandra.sekhar@verse.in>
2023-12-20 18:46:13 +0530

    Paging view support for ios 17 and less

---
+++ PaginationSampleSwiftUI/BottomSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomShetView.swift
+//  PaginationSampleSwiftUI
+//
+//  Created by Chandra Sekhar P V on 19/12/23.
+//
+
+import SwiftUI
+
+struct BottomShetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomShetView()
+}
---
+++ PaginationSampleSwiftUI/PagingScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  PagingScrollView.swift
+//  PaginationSampleSwiftUI
+//
+//  Created by Chandra Sekhar P V on 20/12/23.
+//
+
+import SwiftUI
+
+struct PagingScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PagingScrollView()
+}
