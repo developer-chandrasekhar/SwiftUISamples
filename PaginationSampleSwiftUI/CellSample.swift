625a3f7b3c6a48bd9f16e44275aa233cc36cec38
Chandra Sekhar <chandra.sekhar@verse.in>
2024-01-03 19:38:19 +0530

    sample z stack

---
+++ PaginationSampleSwiftUI/CellSample.swift
@@ -0,0 +1,18 @@
+//
+//  CellSample.swift
+//  PaginationSampleSwiftUI
+//
+//  Created by Chandra Sekhar P V on 03/01/24.
+//
+
+import SwiftUI
+
+struct CellSample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellSample()
+}
