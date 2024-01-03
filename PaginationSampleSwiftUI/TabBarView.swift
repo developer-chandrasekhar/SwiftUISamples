bd85d7eeeb4f2d2eadf6d699b9729c385fa36e3a
Chandra Sekhar <chandra.sekhar@verse.in>
2024-01-03 18:22:29 +0530

    Sample tab bar tabs

---
+++ PaginationSampleSwiftUI/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  PaginationSampleSwiftUI
+//
+//  Created by Chandra Sekhar P V on 03/01/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
